30b0cb63ae54ac44076ec48e57126c76942409bc
Akshita-9931 <sharmatanya9931@gmail.com>
2020-06-04 22:58:54 +0530

    second commit

--- first.java
+++ first.java
@@ -3,15 +3,15 @@ public class first
 {
     public static void main(String args[])
     {
-        int r=0;
+        int s=0;
         Scanner ob=new Scanner(System.in);
         System.out.println("Enter a number");
         int num=ob.nextInt();
         for(int i=num;i>0;i=i/10)
         {
             int d=i%10;
-            r=r*10+d;
+             s=s+d;
         }
-        System.out.println(r);
+        System.out.println(s);
     }
 }
